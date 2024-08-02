8df621bd732bc6ec31911196165d3cb290e5e6f7
VINEEL KRISHNA <vineel183sainik@gmail.com>
2024-08-02 16:04:02 +0530

    Add files via upload

---
+++ dict_remove.py
@@ -0,0 +1,4 @@
+dict = {'Telangana': 'Hyderabad', 'Andhra Pradesh': 'Amaravathi', 'Arunachal Pradesh': 'Itanagar', 'Assam': 'Dispur', 'Goa': 'Panaji', 'Kerala': 'Thiruvanthapuram'}
+remove = print("Enter item to delete : ")
+delete = dict.pop()
+print(dict)
